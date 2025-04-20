ccb6ec8b96c3a77723586dcb0f580918b0e391aa
Beetle <drquinsey@gmail.com>
2025-04-20 08:21:12 -0400

    general cleanup and added functionality to SieveVecBool

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,92 @@
-use rayon::iter::{ParallelBridge, ParallelIterator};
+use rayon::iter::{IntoParallelIterator, ParallelBridge, ParallelIterator};
 
 /**
-A mutable pointer to a bool that we promise to use safely.
+A mutable pointer that we promise to use safely.
 # Safety
 - Modifying the pointer in any way could cause undefined behavior unless you're sure there cannot be race conditions.
 - If only one thread is acting on the pointer you can interact with the pointer in any way you want without worry, but then you may as well just use a pointer instead of this struct.
 */
 #[derive(Debug, Copy, Clone)]
-pub struct ThreadSafeBoolPtr {
-    ptr: *mut bool,
+pub struct ThreadSafeMutPtr<T> {
+    ptr: *mut T,
 }
 
-impl ThreadSafeBoolPtr {
+impl<T> ThreadSafeMutPtr<T> {
     /**
-    Returns a new `ThreadSafeBoolPtr`.
+    Returns a new `ThreadSafeMutPtr`.
     # Safety
     - Calling this function is perfectly safe,
-      but using the `ThreadSafeBoolPtr` it returns is not.
+      but using the `ThreadSafeMutPtr` it returns is not.
     - You MUST know that the way you use the pointer CANNOT cause race conditions.
     */
-    #[allow(unsafe_code)]
-    pub const unsafe fn new(ptr: *mut bool) -> Self {
+    pub const unsafe fn new(ptr: *mut T) -> Self {
         Self { ptr }
     }
+
+    /**
+    Returns a new `ThreadSafeMutPtr`
+    # Safety
+    Has the same safety implications as `ThreadSafeMutPtr::new`
+    */
+    const unsafe fn from_mut_ref(r: &mut T) -> Self {
+        unsafe { Self::new(std::ptr::from_mut(r)) }
+    }
+
+    /**
+    Casts the inner pointer as a mutable reference
+
+    # Panics
+    If the inner pointer is null this panics
+
+    # Safety
+
+    */
+    #[must_use]
+    pub const unsafe fn into_mut_ref<'a>(self) -> Option<&'a mut T> {
+        unsafe { self.into_inner().as_mut() }
+    }
+
+    /**
+    Casts the inner pointer as a mutable reference
+    # Safety
+    The inner pointer must be known to be non-null
+    */
+    #[must_use]
+    pub const unsafe fn into_mut_ref_unchecked<'a>(mut self) -> &'a mut T {
+        unsafe { &mut *self.into_inner() }
+    }
+
     /// Returns the inner pointer
     #[must_use]
-    pub const fn into_inner(self) -> *mut bool {
+    pub const fn into_inner(self) -> *mut T {
         self.ptr
     }
 
-    /// Shorthand for `self.ptr.add(amount)`
-    /// # Safety
-    /// Probably super unsafe but we don't care :3
-    #[allow(unsafe_code)]
+    /**
+    Shorthand for `self.ptr.add(amount)`
+    # Safety
+    Probably super unsafe but we don't care :3
+    */
     #[must_use]
     pub const unsafe fn add(self, amount: usize) -> Self {
         unsafe { Self::new(self.ptr.add(amount)) }
     }
 
-    /// Dereferences the pointer, replacing the bool at that memory address with `false`.
-    #[allow(unsafe_code)]
-    pub const fn write_false(&mut self) {
-        unsafe { self.ptr.write(false) };
+    /// Dereferences the pointer, replacing the value at that address with `new_value`;
+    pub const fn write(&mut self, new_value: T) {
+        unsafe { self.ptr.write(new_value) };
     }
 }
-#[allow(unsafe_code)]
-unsafe impl Send for ThreadSafeBoolPtr {}
-#[allow(unsafe_code)]
-unsafe impl Sync for ThreadSafeBoolPtr {}
 
-/// A data-race safe Vec<bool> where elements default to true and once set to false remain false forever.
-/// # Notes On Safety
-/// For simplicity the internal Vec's length should not grow
+unsafe impl<T> Send for ThreadSafeMutPtr<T> {}
+unsafe impl<T> Sync for ThreadSafeMutPtr<T> {}
+
+/**
+A data-race safe Vec<bool> where elements default to true and once set to false remain false forever.
+# Notes On Safety
+For simplicity the internal Vec's length should not grow
+*/
+#[derive(Debug, Default, Clone)]
 pub struct SieveVecBool {
     vec: Vec<bool>,
 }
@@ -76,11 +111,19 @@ impl SieveVecBool {
     }
 
     /**
+    Returns a `ThreadSafeMutPtr` pointing to the start of the inner `Vec`.
+    # Safety
+    Has the same safety implications as `ThreadSafeMutPtr::new`
+    */
+    unsafe fn ptr_to_start_of_vec(&mut self) -> ThreadSafeMutPtr<bool> {
+        unsafe { ThreadSafeMutPtr::new(self.vec.as_mut_ptr()) }
+    }
+
+    /**
     Sets the element at `index` to false in the `Vec`.
     # Safety
     `index` must be known to be a valid index into the inner `Vec`
     */
-    #[allow(unsafe_code)]
     pub unsafe fn set_false_unchecked(&mut self, index: usize) {
         *unsafe { self.vec.get_unchecked_mut(index) } = false;
     }
@@ -90,20 +133,42 @@ impl SieveVecBool {
     # Safety
     all elements in `(start..stop).step_by(step_size)` must be valid indices into the `Vec`.
     */
-    #[allow(unsafe_code)]
     pub unsafe fn set_step_range_to_false(&mut self, start: usize, stop: usize, step_size: usize) {
-        let ptr: ThreadSafeBoolPtr = unsafe { ThreadSafeBoolPtr::new(self.vec.as_mut_ptr()) };
+        let ptr: ThreadSafeMutPtr<bool> = unsafe { self.ptr_to_start_of_vec() };
 
         let range = (start..stop).step_by(step_size);
         range.par_bridge().for_each(move |index| unsafe {
             let mut p = ptr.add(index);
-            p.write_false();
+            p.write(false);
         });
     }
-}
 
-impl Default for SieveVecBool {
-    fn default() -> Self {
-        Self::new()
+    /**
+    Produces the multiples of `n`, setting all those indices in the inner Vec to false.
+    # Safety
+    Has the same safety implications as `set_step_range_to_false`
+    */
+    pub unsafe fn set_multiples_to_false(&mut self, n: usize) {
+        unsafe { self.set_step_range_to_false(n, self.vec.len(), n) };
+    }
+
+    /**
+    Calls `self.set_multiples_to_false` for all the items in `iter`.
+    # Panics
+    Panics if a null pointer is dereferenced
+
+    # Safety
+    uhhh safety :D
+    */
+    pub unsafe fn set_multiples_of_slice_to_false(&mut self, slice: &[usize]) {
+        let self_ptr: ThreadSafeMutPtr<Self> = unsafe { ThreadSafeMutPtr::from_mut_ref(self) };
+        slice.into_par_iter().for_each(move |&index| {
+            unsafe {
+                self_ptr
+                    .clone()
+                    .into_mut_ref_unchecked()
+                    .set_multiples_to_false(index);
+            };
+        });
     }
 }
