4568773eb57d954a334e4f032180115e94a3e7b0
Beetle <drquinsey@gmail.com>
2025-04-21 09:58:35 -0400

    separated SieveVecBool methods into parallel or non-parallel

--- src/lib.rs
+++ src/lib.rs
@@ -82,9 +82,9 @@ unsafe impl<T> Send for ThreadSafeMutPtr<T> {}
 unsafe impl<T> Sync for ThreadSafeMutPtr<T> {}
 
 /**
-A data-race safe Vec<bool> where elements default to true and once set to false remain false forever.
+A data-race safe `Vec<bool>` where elements default to true and once set to false remain false forever.
 # Notes On Safety
-For simplicity the internal Vec's length should not grow
+For simplicity the internal `Vec`'s length should not grow
 */
 #[derive(Debug, Default, Clone)]
 pub struct SieveVecBool {
@@ -129,11 +129,18 @@ impl SieveVecBool {
     }
 
     /**
-    Calls `set_false` on all the indices in the range given its `start`, `stop`, and `step`
+    Calls `set_false` on all the indices in the range given its `start`, `stop`, and `step`.
+    This differs from `set_step_range_to_false` by performing its operations in parallel, which could be faster depending on your use case.
+
     # Safety
     all elements in `(start..stop).step_by(step_size)` must be valid indices into the `Vec`.
     */
-    pub unsafe fn set_step_range_to_false(&mut self, start: usize, stop: usize, step_size: usize) {
+    pub unsafe fn set_step_range_to_false_par(
+        &mut self,
+        start: usize,
+        stop: usize,
+        step_size: usize,
+    ) {
         let ptr: ThreadSafeMutPtr<bool> = unsafe { self.ptr_to_start_of_vec() };
 
         let range = (start..stop).step_by(step_size);
@@ -141,6 +148,20 @@ impl SieveVecBool {
             let mut p = ptr.add(index);
             p.write(false);
         });
+    }
+
+    /**
+    Calls `set_false` on all the indices in the range given its `start`, `stop`, and `step`.
+
+    # Safety
+    all elements in `(start..stop).step_by(step_size)` must be valid indices into the `Vec`.
+    */
+    pub unsafe fn set_step_range_to_false(&mut self, start: usize, stop: usize, step_size: usize) {
+        let mut index = start;
+        while index < stop {
+            unsafe { self.set_false_unchecked(index) };
+            index += step_size;
+        }
     }
 
     /**
@@ -153,14 +174,24 @@ impl SieveVecBool {
     }
 
     /**
+    Produces the multiples of `n`, setting all those indices in the inner Vec to false.
+    Differs from `set_multiples_to_false` by being parallel, which could be faster depending on your use case.
+
+    # Safety
+    Has the same safety implications as `set_step_range_to_false`
+    */
+    pub unsafe fn set_multiples_to_false_par(&mut self, n: usize) {
+        unsafe { self.set_step_range_to_false_par(n, self.vec.len(), n) };
+    }
+
+    /**
     Calls `self.set_multiples_to_false` for all the items in `iter`.
     # Panics
     Panics if a null pointer is dereferenced
 
     # Safety
-    uhhh safety :D
     */
-    pub unsafe fn set_multiples_of_slice_to_false(&mut self, slice: &[usize]) {
+    pub unsafe fn set_multiples_of_slice_to_false_par(&mut self, slice: &[usize]) {
         let self_ptr: ThreadSafeMutPtr<Self> = unsafe { ThreadSafeMutPtr::from_mut_ref(self) };
         slice.into_par_iter().for_each(move |&index| {
             unsafe {
